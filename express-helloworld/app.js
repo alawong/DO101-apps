40cc609052a3e3f69c7412414fc58f90fe0770e9
alawong <alawong@redhat.com>
2021-05-18 16:06:53 +0100

    Updated app to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,9 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  response = 'This is version 2 of the app' + '\n';
+
+  res.send(response);
 });
 
 app.get('/mars', function (req, res) {
@@ -12,4 +14,3 @@ app.get('/mars', function (req, res) {
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
-
